f5fe4270b51fee4f437a681d1e1a326f003d9d14
INYert <2072302930@qq.com>
2026-04-19 12:15:58 +0800

    T21 合并两个有序链表

--- Leetcode100/Test/A_easy/T21.java
+++ Leetcode100/Test/A_easy/T21.java
@@ -2,20 +2,22 @@ package Test.A_easy;
 
 public class T21 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode result = new ListNode(0);
-        while(list1!=null&&list2!=null){
-            if(list1.val<list2.val){
-                result.next = list1;
+        ListNode left = new ListNode(0);
+        ListNode cur = left;
+        while(list1!=null&&list2!=null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;  //选中两个链表对应数字较小的一个,并且移动选中链表的指针。
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
             }
-            else if(list1.val>list2.val) {
-                result.next = list2;
-            }else{
-                result.next = list1;
-                result.next.next = list2;
-            }
-            list1 = list1.next;
-            list2 = list2.next;
+            cur = cur.next;
         }
-        return result;
+
+        //如果长度不一致，把长的直接接到后面就行了。
+        cur.next = (list1!=null) ? list1 :list2;
+
+        return left.next;
     }
 }

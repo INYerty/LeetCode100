5792bd1c972bc52fc3ba2a886034e0f9c3080ea5
INYert <2072302930@qq.com>
2026-04-28 20:56:59 +0800

    feat(leetcode): 添加二叉树层序遍历和右视图实现
    - 实现T102类中的levelOrder方法，完成二叉树层序遍历功能
    - 使用队列实现BFS算法进行层级遍历
    - 添加TreeNode节点的左右子树处理逻辑
    - 实现T199类中的rightSideView方法，完成二叉树右视图功能
    - 提供递归和层序遍历两种解法实现
    - 完善测试用例和算法边界条件处理

--- Leetcode100/Test/B_medium/T102.java
+++ Leetcode100/Test/B_medium/T102.java
@@ -1,33 +1,39 @@
 package Test.B_medium;
 
+import org.w3c.dom.ls.LSInput;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class T102 {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        if (root !=null) queue.offer(root);
-        List<List<Integer>> lists = new ArrayList<>();
-        while (!queue.isEmpty()){
-            ArrayList<Integer> list = new ArrayList<>();
-            int size = queue.size();
-            while(size!=0){
-                TreeNode node = queue.getFirst();
-                queue.pop();
-                list.add(node.val);
-                size--;
-                if (node.left!=null){
-                    queue.offer(node.left);
-                }
-                if (node.right!=null){
-                    queue.offer(node.right);
-                }
-
-            }
-            lists.add(list);
+        LinkedList<TreeNode> queue = new LinkedList<>();  //声明队列
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        if (root!=null){
+            queue.offer(root);
         }
-        return lists;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            ArrayList<Integer> list = new ArrayList<>();
+            while (size!=0){
+                // 取一层的元素
+                TreeNode first = queue.getFirst();
+                queue.pop();
+                list.add(first.val);
+
+                size--;
+                //如果该元素有左右孩子？
+                if (first.left!=null){
+                    queue.offer(first.left);
+                }
+                if (first.right!=null){
+                    queue.offer(first.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
     }
 
 
--- Leetcode100/Test/B_medium/T199.java
+++ Leetcode100/Test/B_medium/T199.java
@@ -1,10 +1,11 @@
 package Test.B_medium;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class T199 {
-    public List<Integer> rightSideView(TreeNode root) {
+    /*public List<Integer> rightSideView(TreeNode root) {
         ArrayList<Integer> list = new ArrayList<>();
         re(root,0,list);
         return list;
@@ -17,5 +18,27 @@ public class T199 {
         }
         re(node.right,depth+1,list);
         re(node.left,depth+1,list);
+    }*/
+
+/*-----------------------如果用层序遍历呢? BFS--------------------------------*/
+
+    public List<Integer> rightSideView(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList<>(); //声明队列
+        ArrayList<Integer> list = new ArrayList<>();
+        if (root != null) {
+            queue.offer(root);
+        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.right != null) queue.offer(node.right);
+                if (node.left != null) queue.offer(node.left);
+                if (i == 0){
+                    list.add(node.val);
+                }
+            }
+        }
+        return list;
     }
 }

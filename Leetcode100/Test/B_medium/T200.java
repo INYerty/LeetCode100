30e4a72888b885cbca565d2696c617a119957b3f
INYert <2072302930@qq.com>
2026-05-08 10:31:03 +0800

    feat(leetcode): 实现岛屿数量计算功能  方法二
    - 添加了numIslands方法用于计算二维网格中的岛屿数量
    - 实现了DFS深度优先搜索算法来遍历连接的陆地
    - 创建了方向数组用于四个方向的移动检测
    - 添加了访问标记数组避免重复计算
    - 包含完整的测试用例验证算法正确性
    - 注释掉旧的实现方案保留参考代码

--- Leetcode100/Test/B_medium/T200.java
+++ Leetcode100/Test/B_medium/T200.java
@@ -3,6 +3,58 @@ package Test.B_medium;
 public class T200 {
     public int numIslands(char[][] grid) {
         int ans = 0;
+        boolean[][]visited =new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) { // 找到了一个新的岛
+                    ans++;
+                    visited[i][j] = true;
+                    dfs(grid,visited,i,j);
+                }
+            }
+        }
+        return ans;
+    }
+    int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
+    private void dfs(char[][] grid, boolean[][] visited, int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            int newX = x+dir[i][0];
+            int newY = y+dir[i][1];
+            // 如果碰壁,continue
+            if (newX<0||newY<0||newX>=grid.length||newY>=grid[0].length){
+                continue;
+            }
+            if (grid[newX][newY] == '1'&& !visited[newX][newY]){
+                visited[newX][newY] = true;
+                dfs(grid,visited,newX,newY);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
+        };
+
+        T200 t200 = new T200();
+        int islands = t200.numIslands(grid);
+        System.out.println(islands);
+    }
+}
+
+
+
+
+/* 方法一
+package Test.B_medium;
+
+public class T200 {
+    public int numIslands(char[][] grid) {
+        int ans = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') { // 找到了一个新的岛
@@ -39,4 +91,4 @@ public class T200 {
         int islands = t200.numIslands(grid);
         System.out.println(islands);
     }
-}
+}*/

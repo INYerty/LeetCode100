16152cc523d451af7c886e9891648b971889a481
INYert <2072302930@qq.com>
2026-05-15 16:19:29 +0800

    ``` feat(algorithm): 实现数组全排列的回溯算法
    - 添加了布尔数组标记已使用的元素
    - 实现了回溯递归方法处理排列逻辑
    - 在递归终止条件时将当前路径加入结果集
    - 遍历所有可能位置并进行选择和撤销操作
    - 完成了全排列问题的核心算法实现
    ```

--- Leetcode100/Test/B_medium/T46.java
+++ Leetcode100/Test/B_medium/T46.java
@@ -6,12 +6,29 @@ import java.util.List;
 public class T46 {
 
     public List<List<Integer>> permute(int[] nums) {
-
+        boolean[] used = new boolean[nums.length];
+        backTrace(nums,used);
+        return result;
     }
+
     List<List<Integer>> result = new ArrayList<>();
     List<Integer> path = new ArrayList<>();
 
-    public void backTracing(int[] nums){
+    public void backTrace(int[] nums,boolean[] used){
+        if(path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
 
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]){
+                path.add(nums[i]);
+                used[i] = true;
+                backTrace(nums,used);
+                used[i] = false;
+                path.removeLast();
+            }
+
+        }
     }
 }

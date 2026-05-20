9960310ed84f5318381991e275359b04acd98b93
INYert <2072302930@qq.com>
2026-05-20 10:27:21 +0800

    refactor(T79): 重构单词搜索算法实现
    - 移除原有的注释代码并重新实现完整的回溯算法
    - 添加visited二维数组来跟踪已访问的位置
    - 实现新的backTrace递归方法进行深度优先搜索
    - 添加边界检查和路径回溯逻辑
    - 优化字符匹配和队列操作流程
    - 完善四方向移动的坐标计算和验证机制

--- Leetcode100/Test/B_medium/T79.java
+++ Leetcode100/Test/B_medium/T79.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 public class T79 {
     public boolean exist(char[][] board, String word) {
-        // 将wordToCharArray之后入栈，每找到一个元素pop 并标记visited二维数组为true。每一行的元素
+        /*// 将wordToCharArray之后入栈，每找到一个元素pop 并标记visited二维数组为true。每一行的元素
         char[] charArray = word.toCharArray();
         LinkedList<Character> queue = new LinkedList<>();
         for(char i:charArray){
@@ -23,10 +23,36 @@ public class T79 {
                 }
             }
         }
+        return false;*/
+        boolean[][] visited = new boolean[board.length][board[0].length];
+
+        LinkedList<Character> queue = new LinkedList<>();
+        char[] charArray = word.toCharArray();
+        for(char i :charArray){
+            queue.add(i);
+        }
+
+        // 寻找起始点
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (!queue.isEmpty()){
+                    Character peek = queue.peek();
+                    if(board[i][j] == peek){
+                        visited[i][j] = true;
+                        queue.pop();
+                        if (backTrace(board,visited,queue,i,j)) return true;
+                        visited[i][j] = false;
+                        queue.addFirst(peek);
+                    }
+                }
+            }
+        }
         return false;
+
+
     }
 
-    //四方向
+    /*//四方向
     int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
     public boolean backTrace(char[][] board,LinkedList<Character> queue,boolean[][] visited,int x,int y){
@@ -48,6 +74,24 @@ public class T79 {
                 queue.addFirst(peek);
             }
         }
+        return false;
+    }*/
+
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    public boolean backTrace(char[][] board,boolean[][] visited,LinkedList<Character> queue,int x,int y){
+        if (queue.isEmpty()) return true;
+        for (int i = 0; i < 4; i++) {
+            int newX = x + dirs[i][0];
+            int newY = y + dirs[i][1];
+            // 判断越界 + visited 判断 + 不等值判断
+            if (newX<0 || newY<0 || newX>=board.length || newY>=board[0].length || visited[newX][newY] || board[newX][newY] != queue.peek()) continue;
+            visited[newX][newY] = true;
+            Character poll = queue.poll();
+            if (backTrace(board,visited,queue,newX,newY)) return true;
+            visited[newX][newY] = false;
+            queue.addFirst(poll);
+        }
+
         return false;
     }
 }

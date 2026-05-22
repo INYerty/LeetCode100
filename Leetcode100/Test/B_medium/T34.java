6ecd58686a2b8c43a3ff4a38ac8440eafc46486f
INYert <2072302930@qq.com>
2026-05-22 23:56:31 +0800

    添加 中等题 34. 在排序数组中查找元素的第一个和最后一个位置做法
    添加 中等题 34. 在排序数组中查找元素的第一个和最后一个位置的二分查找做法

---
+++ Leetcode100/Test/B_medium/T34.java
@@ -0,0 +1,30 @@
+public class T34 {
+    public int[] searchRange(int[] nums, int target) {
+        int leftSite = binarySearch(nums,target,true);
+        int rightSite = binarySearch(nums,target,false);
+        return new int[] {leftSite,rightSite};
+    }
+
+    // 寻找左边界
+    public int binarySearch(int[] nums,int target,boolean isLeft){
+        int left = 0,right = nums.length-1;
+        int index = -1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if (target>nums[mid]){
+                left = mid+1;
+            } else if (target<nums[mid]) {
+                right = mid-1;
+            }else{
+                index = mid;
+                if(isLeft){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+        }
+        return index;
+    }
+
+}

e888d7baa4df1503e012e7869fbfa31436e4cf89
INYert <2072302930@qq.com>
2026-05-22 23:17:12 +0800

    Comment out alternative searchMatrix method
    Commented out an alternative searchMatrix method implementation.

--- Leetcode100/Test/B_medium/T74.java
+++ Leetcode100/Test/B_medium/T74.java
@@ -25,4 +25,29 @@ public class T74 {
         }
         return false;
     }
+    /*
+    // 方法二
+        public boolean searchMatrix(int[][] matrix, int target) {
+        int left = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                list.add(matrix[i][j]);
+            }
+        }
+        int right = list.size()-1;
+        int result = 0;
+        while(left<right){
+            int mid = (left+right)/2;
+            if (target<list.get(mid)){
+                right = mid-1;
+            } else if (target>list.get(mid)) {
+                left = mid+1;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+    */
 }

be37f164ee7f870d710aaba6b9c9041c836e0884
INYert <2072302930@qq.com>
2026-06-03 15:34:34 +0800

    feat(leetcode): 添加第215题数组中第K个最大元素的解决方案
    - 实现堆排序算法解决寻找数组中第K个最大元素问题
    - 添加小顶堆构建和维护功能
    - 实现堆化过程中的元素比较和交换逻辑
    - 添加寻找第K大元素的优化算法
    - 注释掉暴力排序和完整堆排序的备用方案

---
+++ Leetcode100/Test/B_medium/T215.java
@@ -0,0 +1,99 @@
+package Test.B_medium;
+
+
+public class T215 {
+
+    // 大顶堆
+    /*void heapify(int[] nums,int n,int root){
+        int largest = root;
+        int lson = root * 2 + 1;
+        int rson = root * 2 + 2;
+        if(lson < n && nums[largest] < nums[lson]){
+            largest = lson;
+        }
+        if (rson < n && nums[largest] < nums[rson]){
+            largest = rson;
+        }
+        // 发生了变化 ，我们要swap
+        if (largest != root) {
+            // 交换nums[largest] 与 nums[root]
+            swap(nums, largest, root);
+            // 递归他们的孩子 是否也符合大顶堆
+            heapify(nums, n, largest);
+        }
+    }*/
+
+    void heapify(int[] nums,int n,int root){
+        int less = root;
+        int lson = root * 2 + 1;
+        int rson = root * 2 + 2;
+        if(lson < n && nums[less] > nums[lson]){
+            less = lson;
+        }
+        if (rson < n && nums[less] > nums[rson]){
+            less = rson;
+        }
+        // 发生了变化 ，我们要swap
+        if (less != root) {
+            // 交换nums[less] 与 nums[root]
+            swap(nums, less, root);
+            // 递归他们的孩子 是否也符合小顶堆
+            heapify(nums, n, less);
+        }
+    }
+
+    // 堆排序 n是堆的大小
+    void heap_sort(int[] nums,int n){
+        int i;
+        // 建堆
+        // 这里的n是数组大小  for用的是下标 因此-1
+        for(i = (n-1-1)/2;i>=0;i--){
+            heapify(nums, n, i);
+        }
+
+        // 排序
+        for (int j = n-1; j > 0 ; j--) {
+            swap(nums, j,0);
+            heapify(nums, j, 0);
+        }
+    }
+
+    // 交换方法
+    void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+/*    public static void main(String[] args) {
+        T215 t = new T215();
+        int[] nums = {2,3,8,1,4,9,10,7,16,14};
+        t.heap_sort(nums,nums.length);
+        System.out.println(Arrays.toString(nums));
+    }*/
+
+
+    public int findKthLargest(int[] nums, int k) {
+        // 暴力  排序
+/*        Arrays.sort(nums);
+        return nums[nums.length - k];*/
+
+        // 最小堆排序
+        /*heap_sort(nums, nums.length);
+        return nums[k];*/
+
+        // 维护K大小的小顶堆
+        for (int i = k/2 - 1; i >= 0; i--) {
+            heapify(nums, k, i);
+        }
+
+        // 剩下的元素
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > nums[0]){
+                nums[0] = nums[i];
+                heapify(nums, k, 0);
+            }
+        }
+        return nums[0];
+    }
+}

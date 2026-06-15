8402b99b348d704f1007c1857ee9c41ae187b8cf
INYert <2072302930@qq.com>
2026-06-15 11:20:33 +0800

    ``` docs(T738): 添加单调递增数字问题描述注释
    - 添加了完整的题目描述和说明
    - 包含了问题标签和相关企业信息
    - 添加了题目链接和详细解释
    ```

--- Leetcode100/Test/B_medium/T738.java
+++ Leetcode100/Test/B_medium/T738.java
@@ -1,6 +1,19 @@
 package Test.B_medium;
 
-import java.util.Arrays;
+/**
+ * 738. 单调递增的数字
+ * 已解答
+ * 中等
+ * 相关标签
+ * premium lock icon
+ * 相关企业
+ * 提示
+ * 当且仅当每个相邻位数上的数字 x 和 y 满足 x <= y 时，我们称这个整数是单调递增的。
+ *
+ * 给定一个整数 n ，返回 小于或等于 n 的最大数字，且数字呈 单调递增 。
+ *
+ * https://leetcode.cn/problems/monotone-increasing-digits/description/
+ */
 
 public class T738 {
     public int monotoneIncreasingDigits(int n) {

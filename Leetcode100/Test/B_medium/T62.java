21888c56a5adc8d1e5a4c7e27a5a91726a4698ed
INYert <2072302930@qq.com>
2026-06-19 23:25:28 +0800

    feat(Leetcode100): 添加机器人不同路径问题解决方案
    - 创建 T62 类用于解决 LeetCode 62 题
    - 实现 uniquePaths 方法计算从左上角到右下角的不同路径数
    - 添加题目描述注释和题目标签
    - 暂时返回 0，待后续实现具体算法逻辑

---
+++ Leetcode100/Test/B_medium/T62.java
@@ -0,0 +1,18 @@
+package Test.B_medium;
+
+/**
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ *
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ *
+ * 问总共有多少条不同的路径？
+ *
+ *https://leetcode.cn/problems/unique-paths/description/
+ */
+
+public class T62 {
+    //TODO 明天在做 今天来济南玩了
+    public int uniquePaths(int m, int n) {
+        return 0;
+    }
+}

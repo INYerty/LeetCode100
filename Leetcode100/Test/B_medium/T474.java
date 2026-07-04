069c0a9c832c5e5e3dc362fc29217e3476b6395c
INYert <2072302930@qq.com>
2026-07-05 00:38:40 +0800

    feat(Leetcode100): 新增474题解决方案并完善494题描述
    - 添加474题一和零问题的Java实现框架
    - 补充494题目标和问题的完整题目描述信息
    - 实现474题findMaxForm方法的基本结构
    - 完善T494类的注释文档和问题说明

---
+++ Leetcode100/Test/B_medium/T474.java
@@ -0,0 +1,26 @@
+package Test.B_medium;
+/**
+ * 474. 一和零
+ * 中等
+ * 相关标签
+ * premium lock icon
+ * 相关企业
+ * 给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+ *
+ * 请你找出并返回 strs 的最大子集的长度，该子集中 最多 有 m 个 0 和 n 个 1 。
+ *
+ * 如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
+ *
+ * https://leetcode.cn/problems/ones-and-zeroes/description/
+ */
+public class T474 {
+    public int findMaxForm(String[] strs, int m, int n) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        T474 t474 = new T474();
+        String[] str = {"10", "0001", "111001", "1", "0"};
+        t474.findMaxForm(str,5,3);
+    }
+}
--- Leetcode100/Test/B_medium/T494.java
+++ Leetcode100/Test/B_medium/T494.java
@@ -1,5 +1,21 @@
 package Test.B_medium;
 
+/**
+ * 494. 目标和
+ * 已解答
+ * 中等
+ * 相关标签
+ * premium lock icon
+ * 相关企业
+ * 给你一个非负整数数组 nums 和一个整数 target 。
+ *
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ *
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ *
+ * https://leetcode.cn/problems/target-sum/description/
+ */
 public class T494 {
     public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
